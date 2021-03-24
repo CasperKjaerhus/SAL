39845a4051b0957a8240b578c19f7d7a55a726a1
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2021-03-24 13:47:39 +0100

    removes folder + run grun script

--- Antlr/compile_and_run.bat
+++ Antlr/compile_and_run.bat
@@ -2,4 +2,6 @@
 @RD /S /Q "./ParseGen"
 java org.antlr.v4.Tool p4.g4 -o ./ParseGen
 javac --release 8 ./ParseGen/*.java
+cd ./ParseGen
+grun p4 s -gui
 PAUSE
---
+++ Antlr/run_grun.bat
@@ -0,0 +1,4 @@
+@echo off
+cd ./ParseGen
+grun p4 s -gui
+PAUSE
