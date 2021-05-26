46176152d2aaf9b7a5a8479ea9b545a5b9502900
anesheim-uni <57893968+anesheim-uni@users.noreply.github.com>
2021-05-26 14:08:33 +0200

    Can now quit shell

---
+++ Shell/SALShell/SALShell/Command/ExitCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using SALShell.Core;
+using SALShell.UI;
+
+namespace SALShell.Command
+{
+    class ExitCommand :ICommand
+    {
+        public void Execute(string[] argStrings, IShellUI ui, ICore core)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
